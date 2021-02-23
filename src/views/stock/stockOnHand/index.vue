57c722d58d00d6f2d505b1e88985934b23b6522f
yangxia <yangxia@haier.com>
2021-02-23 14:44:52 +0800

    根据左侧菜单栏，配置路由及其对应的vue文件，注：admin文件夹没有删除，可以当作框架的模板使用

---
+++ src/views/stock/stockOnHand/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        现存量页面。。。。。。
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+    // components: { ComponentName },
+    // directives: { DirectiveName },
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
